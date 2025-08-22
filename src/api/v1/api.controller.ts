ffd237ab1297df38aa756f2373e58b358a60b141
WongYC-66 <yengchien@hotmail.com>
2025-08-22 12:18:17 +0800

    draft : api/v1/controller

--- src/api/v1/api.controller.ts
+++ src/api/v1/api.controller.ts
@@ -1,20 +1,62 @@
 
 // import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
-import { Body, Controller, Get, HttpException, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('api/v1')
 export class ApiController {
 
-    constructor(private PrismaService: PrismaService) { }
+    constructor(private prisma: PrismaService) { }
 
     @Get("/outlets")
     getOutlets() {
-        return "a"
+        return this.prisma.outlet.findMany();
     }
 
-    // constructor(private catsService: CatsService) { }
+    @Get("/outlets/:outlet_id")
+    getOutletById(@Param(":outlet_id") outlet_id: string) {
+        console.log(outlet_id)
+        return this.prisma.outlet.findFirst({
+            where: {
+                id: {
+                    equals: Number(outlet_id)
+                }
+            }
+        });
+    }
+
+    @Post("/users/login")
+    postLogin() {
+        return "todo: login user and return a simple token";
+    }
+
+    @Get("/foods")
+    getFoods() {
+        return this.prisma.food.findMany();
+    }
+
+    @Get("/outlets/:outlet_id")
+    getFoodById() {
+        return "todo: return 1 food detail";
+    }
+
+    @Get("/orders")
+    getOrders() {
+        return "todo: return all order accesible by user";
+    }
+
+    @Get("/orders/:order_id")
+    getOrderById() {
+        return "todo: return one order accesible by user";
+    }
+
+    @Post("/orders")
+    postOneOrder() {
+        return "todo: make a new order by user";
+    }
+
+
 
     // @Post()
     // async create(@Body() createCatDto: CreateCatDto) {
